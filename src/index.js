eb9f4665f9a951cf342374d97714c8bdb8aef76a
Android Studio <devandroid@ort>
2025-06-09 10:25:44 -0300

    BD conectada

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import express from "express";
+const express = require("express");
+const { createClient } = require("@supabase/supabase-js");
 
 const app = express();
 const port = 3000;
 
+const supabase = createClient(
+  'https://hcsjfvirqtjmvkmqngsg.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhjc2pmdmlycXRqbXZrbXFuZ3NnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwMDk1MDEsImV4cCI6MjA2MzU4NTUwMX0.ujybtGbUqnvcPkriZ4oA5Zy-ijNMG53o2uKxdgTTi74'
+);
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Backend conectado a Supabase");
 });
 
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Servidor escuchando en http://0.0.0.0:${port}`);
+app.get("/generos", async (req, res) => {
+  const { data, error } = await supabase.from('gender').select('*');
+
+  if (error) {
+    return res.status(500).json({ error: error.message });
+  }
+
+  res.json(data);
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
 });
